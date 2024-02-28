3d89a3a4a01806f539a05707dfe5c2882112341a
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 15:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.querySelector('button').addEventListener('click',(e)=>{
+	e.preventDefault();
+	let content=document.getElementsByClassName('content')[0];
+	content.innerText='Entered the Metaverse';
+})
